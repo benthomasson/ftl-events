837a71b8a446600636b6ad966887f237c4f27024
Ben Thomasson <bthomass@redhat.com>
2022-01-28 15:31:44 -0500

    Bump version: 0.1.7 → 0.1.8

--- python-asyncio/ftl_events/setup.py
+++ python-asyncio/ftl_events/setup.py
@@ -44,7 +44,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/benthomasson/ftl_events',
-    version='0.1.7',
+    version='0.1.8',
     zip_safe=False,
     entry_points={
         'console_scripts': [
